d7172ca81904750c032894b767ef5e4922ce3733
Tamir Yahav <tamir.yahav555@gmail.com>
2023-06-12 23:28:28 +0300

    finished

--- controller/students.js
+++ controller/students.js
@@ -1,37 +1,45 @@
 import express from "express";
-const router = express.Router()
-import user from '../models/user.js'
+const router = express.Router();
+import user from "../models/user.js";
 
-router.get('/postStudents',(req,res) => {
-    res.render('postStudents')
-})
-router.get('/deleteStudents',(req,res) => {
-    res.render('deleteStudents')
-})
+router.get("/postStudents", (req, res) => {
+  res.render("postStudents");
+});
 
+router.get("/deleteStudents", (req, res) => {
+  res.render("deleteStudents");
+});
 
-router.post('/postStudents',(req,res) => {
-
-    const userDetails = req.body
-  user.create(userDetails).then( d => {
-    res.redirect('/')
-    
- })
-})
-
-
-
-
-router.delete('/:firstName',(req,res) => {
-    const userName = req.params.firstName
-    user.destroy({
-        where:{firstName:userName}
-    }).then( d => {
-        res. res.redirect('/')
+router.get("/deleteUser/:id", (req, res) => {
+  const userId = req.params.id;
+  user
+    .findByPk(userId)
+    .then((user) => {
+      user.destroy().then((user_deleted) => {
+        res.redirect("/");
+      });
     })
-})
+    .catch((err) => {
+      console.log(err.message);
+      res.redirect("/");
+    });
+});
 
+router.post("/postStudents", (req, res) => {
+  const userDetails = req.body;
+  if ( userDetails.firstName.length === 0 || userDetails.lastName.length === 0  || userDetails.email.length === 0 || userDetails.tel.length === 0) {
+     return res.status(400).send("Please fill in all the fields.");
+  }
 
+  user
+    .create(userDetails)
+    .then((d) => {
+      res.redirect("/");
+    })
 
+    .catch((error) => {
+      res.status(500).send("An error occurred while adding the student.");
+    });
+});
 
-export default router
+export default router;
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import studentsRoute from "./controller/students.js"
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-
+app.use('/public', express.static('public'))
 app.set('view engine', 'ejs')
 
 
